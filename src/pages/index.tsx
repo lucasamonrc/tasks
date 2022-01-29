3e0c37df3a4062cb0ae1437c2494a1b1d984e437
lucasamonrc <lucasamonrc@gmail.com>
2022-01-29 15:49:20 -0700

    style home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,54 @@
+import { FiTrash2 } from 'react-icons/fi'
+
+import styles from '../styles/pages/Home.module.css';
+
 export default function Home() {
   return (
     <div>
-      <header>
+      <header className={styles.header}>
         <nav>
-          <h1>✅ Tasks</h1>
+          <h1>✏️ Tasks</h1>
           <button>logout</button>
         </nav>
       </header>
 
-      <section>
+      <section className={styles.container}>
         <header>
           <h2>To do:</h2>
 
           <section>
             <div>
-              <input type="checkbox" />
-              <label>Sort by date</label>
+              <input type="checkbox" className="switch" />
+              <label>{' '}Sort by date</label>
             </div>
 
             <div>
-              <input type="checkbox" />
-              <label>Filter completed</label>
+              <input type="checkbox" className="switch" />
+              <label>{' '}Filter completed</label>
             </div>
           </section>
         </header>
 
         <main>
-          <ul>
+          <ul className={styles.tasks}>
             <li>
               <div>
-                <input type="checkbox" />
-                <label>Some task</label>
+                <input id="1" type="checkbox" className="todo" />
+                <label htmlFor="1">Some task</label>
               </div>
-              <time>2020-01-01</time>
-              <button>delete</button>
+              <div>
+                <time>2020-01-01</time>
+                <button><FiTrash2 size={16} /></button>
+              </div>
             </li>
           </ul>
         </main>
 
         <footer>
           <form>
-            <input type="text" required />
+            <input type="text" placeholder="Task description" required />
             <input type="date" required />
-            <button type="submit"></button>
+            <button type="submit">Add task</button>
           </form>
         </footer>
       </section>
