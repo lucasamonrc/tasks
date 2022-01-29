aec44c385d603a446b572e4df7e5a55dd01e0898
lucasamonrc <lucasamonrc@gmail.com>
2022-01-29 14:50:02 -0700

    add markup to home page

---
+++ src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet" />
+          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,51 @@
 export default function Home() {
   return (
     <div>
-      <h1>Tasks</h1>
+      <header>
+        <nav>
+          <h1>✅ Tasks</h1>
+          <button>logout</button>
+        </nav>
+      </header>
+
+      <section>
+        <header>
+          <h2>To do:</h2>
+
+          <section>
+            <div>
+              <input type="checkbox" />
+              <label>Sort by date</label>
+            </div>
+
+            <div>
+              <input type="checkbox" />
+              <label>Filter completed</label>
+            </div>
+          </section>
+        </header>
+
+        <main>
+          <ul>
+            <li>
+              <div>
+                <input type="checkbox" />
+                <label>Some task</label>
+              </div>
+              <time>2020-01-01</time>
+              <button>delete</button>
+            </li>
+          </ul>
+        </main>
+
+        <footer>
+          <form>
+            <input type="text" required />
+            <input type="date" required />
+            <button type="submit"></button>
+          </form>
+        </footer>
+      </section>
     </div>
   );
 }
